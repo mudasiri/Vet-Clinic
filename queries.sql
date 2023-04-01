50d8de81649a84294647191ac30347231a0e25a6
Issah Mudasir <danladi496@gmail.com>
2023-04-01 22:08:46 +0000

    Added-Screenshots

--- queries.sql
+++ queries.sql
@@ -41,3 +41,48 @@ SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
 SELECT MAX(weight_kg) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+
+---What animals belong to Melody Pond?---
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+---List of all animals that are pokemon (their type is Pokemon).--
+SELECT animals.name
+FROM animals
+JOIN species ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+---List all owners and their animals, remember to include those that don't own any animal.--
+SELECT owners.full_name, animals.name
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owners_id;
+
+---How many animals are there per species?--
+SELECT species.name, COUNT(animals.id) 
+FROM species 
+JOIN animals ON species.id = animals.species_id 
+GROUP BY species.name;
+
+---List all Digimon owned by Jennifer Orwell.--
+SELECT animals.name
+FROM animals
+JOIN species ON animals.species_id = species.id
+JOIN owners ON animals.owner_id = owners.id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+
+--- List all animals owned by Dean Winchester that haven't tried to escape.--
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+
+---Who owns the most animals?--
+SELECT owners.full_name, COUNT(animals.id)
+FROM owners
+JOIN animals ON owners.id = animals.owner_id
+GROUP BY owners.full_name
+ORDER BY COUNT(animals.id) DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -11,3 +11,29 @@ CREATE TABLE animals (
 
 /* Alter Tables Animal */
 ALTER TABLE animals ADD COLUMN species varchar;
+
+/*Create owners table  */
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INT  
+);
+/* Create the species table */
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+/* Modify animals table*/
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
